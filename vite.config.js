3a52d1dd1dc3f32cdebd00bae28135a9b40b44d1
mdabuzafar <mdabuzafar78@gmail.com>
2025-10-24 10:39:23 +0530

    chore: configure path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig, transformWithEsbuild } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig, transformWithEsbuild } from 'vite';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@api': path.resolve(__dirname, './src/api.js'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
   plugins: [
     {
       name: 'treat-js-files-as-jsx',
